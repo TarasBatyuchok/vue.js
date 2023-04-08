b0b38dea6345b28b68b88fa532f466a4e33c340c
Taras  Batyuchok <57962357+TarasBatyuchok@users.noreply.github.com>
2023-04-08 17:11:49 +0300

    add styles+audio

---
+++ hw-2/into-js/audio.js
@@ -0,0 +1,30 @@
+let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// check browser
+
+
+
+
+
+
+let track = document.getElementById('track');
+
+let controlBtn = document.getElementById('play-pause');
+
+function playPause() {
+    if (track.paused) {
+        track.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else { 
+        track.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function() {
+  controlBtn.className = "play";
+});
+
+
